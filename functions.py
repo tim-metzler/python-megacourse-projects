af9817fc6bdb11bcde05505258b53275298db272
tim-metzler <tim-metzler@web.de>
2023-10-30 13:11:51 +0100

    Implement the ADd button

--- functions.py
+++ functions.py
@@ -5,7 +5,7 @@ def get_todos(filepath="todos.txt"):
     return todos_local
 
 
-def write_todos(filepath="todos.txt"):
+def write_todos(todos_arg, filepath="todos.txt"):
     """Writes todos to a text file"""
     with open(filepath, "w") as file_local:
-        file_local.writelines(todos)
+        file_local.writelines(todos_arg)

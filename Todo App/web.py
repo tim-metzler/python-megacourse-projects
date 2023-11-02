7e6ae4e519a78e95a5990726ba3212b5de4d57df
tim-metzler <tim-metzler@web.de>
2023-11-02 19:20:35 +0100

    implement removing todos functionality

--- Todo App/web.py
+++ Todo App/web.py
@@ -18,8 +18,13 @@ st.title("My Todo App")
 st.subheader("This is my todo app")
 st.write("Do some of the stuff on this list")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input("", placeholder="Enter a new todo",
               on_change=add_todo, key='new_todo')
